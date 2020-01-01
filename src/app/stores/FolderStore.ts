ec98b181350d98623797ad49e4f375b209cf5b45
andrew <andrew@textile.io>
2019-12-31 17:59:08 -0800

    updates listen method; bumps to client 0.2.1
    Signed-off-by: andrew <andrew@textile.io>

--- src/app/stores/FolderStore.ts
+++ src/app/stores/FolderStore.ts
@@ -93,12 +93,9 @@ export class FolderStore {
     const found = await this.client.modelFindByID(this.finderID, 'Folder2', this.folderID)
     this.folder = new FolderModel(found.entity)
 
-    await this.client.listen(this.finderID, 'Folder2', this.folderID, ((reply) => {
-      console.debug('Folder updated...')
-      // @todo: this updated model does not contain the new files.
-      // this.folder = new FolderModel(found.entity as string)
-      // console.debug(this.folder)
-    }))
+    await this.client.listen<FolderModel>(this.finderID, 'Folder2', this.folderID, reply => {
+      console.debug('Folder updated...', reply.entity.ID)
+    })
   }
 
   @action public async addFile(domFile: File) {
